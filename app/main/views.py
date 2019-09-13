77ae33762493e3e7a101b6d11587460c8a0bfbda
Apamela <pamelab.desire@gmail.com>
2019-09-13 20:03:57 +0200

    add the articles

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-# from ..requests import get_sources,get_articles
+from ..requests import get_sources
 from ..models import Sources
 
 # Views
@@ -10,9 +10,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    sports_sources= get_sources('sports')
-    technology_sources= get_ sources('technology')
+    sources= get_sources('sports')
+    technology_sources= get_sources('technology')
     business_sources= get_sources('business')
     politics_sources= get_sources('politics')
     title="New-highlight"
-    return render_template('index.htm',title=tltle,sports=sports_sources,technology=technology_sources,business=business_sources,politics=politics_sources)
+    return render_template('index.htm',title=tltle,sources=sources,technology=technology_sources,business=business_sources,politics=politics_sources)
