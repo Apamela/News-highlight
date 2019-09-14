0ad9e33103fa02627926dd9b8ee41fcc2bd0d6e7
Apamela <pamelab.desire@gmail.com>
2019-09-14 14:03:39 +0200

    addthe  file

--- app/main/views.py
+++ app/main/views.py
@@ -10,12 +10,12 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    sources= get_sources('sports')
+    entertainment_sources= get_sources('entertainment')
     technology_sources= get_sources('technology')
     business_sources= get_sources('business')
     politics_sources= get_sources('politics')
     title="News-highlight"
-    return render_template('index.htm',title=tltle,sources=sources,technology=technology_sources,business=business_sources,politics=politics_sources)
+    return render_template('index.htm',title=tltle,technology=technology_sources,business=business_sources,politics=politics_sources)
 
 @main.route('/sources/<id>')
 def articles(id):
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
 
-    NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?id={}api_Key={}'
+    NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?category={}&api_Key={}'
     ARTICLES_BASE_URL='https://newsapi.org/v2/everything?id={}api_Key={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
